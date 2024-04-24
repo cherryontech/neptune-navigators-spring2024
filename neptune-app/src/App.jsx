bb7f875e9921926f84428eef8ef6e52e9019b2c6
Sammy Tran <sammy.tran1@ucalgary.ca>
2024-04-24 16:56:12 -0600

    Added draftui

--- neptune-app/src/App.jsx
+++ neptune-app/src/App.jsx
@@ -1,36 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { RouterProvider } from "react-aria-components";
 import "./App.css";
+import HomePage from "./Pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
+  let navigate = useNavigate()
 
   return (
-    <>
-      <div className="flex justify-center">
-        <a href="https://vitejs.dev" target="blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <h1 className="text-3xl font-bold underline text-blue-700">
-        Hello, we&apos;re the Neptune Navigators!
-      </h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider navigate={navigate}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </RouterProvider>
   );
 }
 
