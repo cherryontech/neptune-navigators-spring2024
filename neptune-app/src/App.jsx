0796d1a1f1747c84def6c028144697329600d45f
IM <39933650+imadrig@users.noreply.github.com>
2024-04-30 22:23:17 -0400

    Add Site Header and NavBar

--- neptune-app/src/App.jsx
+++ neptune-app/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import { RouterProvider } from "react-aria-components";
 import "./App.css";
 import HomePage from "./Pages/Home";
+import Header from './components/PageLayout/Header';
 
 function App() {
   let navigate = useNavigate()
 
   return (
     <RouterProvider navigate={navigate}>
+      <Header/>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
