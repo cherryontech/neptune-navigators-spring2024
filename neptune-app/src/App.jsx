f1dd11870aefe899fae963fd9c92684eab341654
Sammy Tran <sammy.tran1@ucalgary.ca>
2024-05-02 10:53:15 -0600

    Add index page

--- neptune-app/src/App.jsx
+++ neptune-app/src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { RouterProvider } from "react-aria-components";
 import "./App.css";
 import HomePage from "./Pages/Home";
+import IndexPage from "./Pages/Index";
 
 function App() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   return (
     <RouterProvider navigate={navigate}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/index" element={<IndexPage />} />
       </Routes>
     </RouterProvider>
   );
---
+++ neptune-app/src/Pages/Index.jsx
@@ -0,0 +1,17 @@
+import { Button } from "../draftuiComponents/button";
+
+function IndexPage() {
+  return (
+    <>
+      <h1> Hi! We&apos;re [insert brand name]</h1>
+      <p>
+        We&apos;re here to help you advance your career goals. Start by taking
+        our skills assessment to see what skills you already possess and what
+        skills we can help you improve upon.
+      </p>
+      <Button variant="subtle"> Start now </Button>
+    </>
+  );
+}
+
+export default IndexPage;
